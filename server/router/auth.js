849b8f452c770628239cc6957c1046f9554c7a1f
Lokesh Shah6 <lokshah6@in.ibm.com>
2021-07-21 19:41:51 +0530

    Commit two

--- server/router/auth.js
+++ server/router/auth.js
@@ -52,17 +52,24 @@ router.get('/', (req, res) => {
             const userExist = await User.findOne({ email: email })
 
             if (userExist) {
+               
                 return res.status(422).json({ error: "email exist" });
+
+            }else if ( password != cpassword){
+                
+                return res.status(422).json({ error: "enter coorect password" });  
+
+            }else{
+
+                const user = new User({ name, email, phone, work, password, cpassword })
+
+                // const userRegister = await user.save();
+    
+                await user.save();
+        
+                res.status(201).json({message: " User registered successfully "});
+
             }
-            
-            const user = new User({ name, email, phone, work, password, cpassword })
-    
-            // const userRegister = await user.save();
-
-            await user.save();
-    
-            res.status(201).json({message: " User registered successfully "});
-
             // if(userRegister){
             //     res.status(201).json({message: " User registered successfully "});
             // }
@@ -72,5 +79,37 @@ router.get('/', (req, res) => {
         }
        
 });
+
+
+//login route 
+
+router.post('/signin', async (req, res) => {
+    
+    // console.log(req.body);
+    // res.json({ message:"awesome"});
+    try{
+        const { email,password } =req.body;
+
+        if( !email || !password){
+
+            return res.status(400).json({error:"please filled the data"});
+        }
+
+        //how to read database data from
+
+        const userLogin = await User.findOne({ email:email });
+
+        if(!userLogin){
+            res.status(400).json({error:"User error"});
+        }else{
+            res.json({message:"user signin successfully"});
+        }
+       
+
+    }catch (err) {
+        console.log(err);
+    }
+
+})
 
 module.exports = router;
